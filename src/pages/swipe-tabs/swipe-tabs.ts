a1aa178c00a0219e3d7ad8ec0fccce1427de70d9
Geoffrey Pay <geoffrey.pay@gmail.com>
2017-10-28 15:32:12 -0700

    Added ActionSheet triggered by FAB

--- src/pages/swipe-tabs/swipe-tabs.ts
+++ src/pages/swipe-tabs/swipe-tabs.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams, Slides } from 'ionic-angular';
+import { NavController, NavParams, Slides, Platform, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-swipe-tabs',
@@ -10,7 +10,12 @@ export class SwipeTabsPage {
   @ViewChild('pageSlider') pageSlider: Slides; 
   tabs: any = '0';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}  
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public platform: Platform,
+    public actionSheetCtrl: ActionSheetController
+  ) {}  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SwipeTabsPage');
@@ -18,4 +23,52 @@ export class SwipeTabsPage {
   selectTab(index) {
     this.pageSlider.slideTo(index);
   }
+
+  openMenu() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Applications',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
 }
